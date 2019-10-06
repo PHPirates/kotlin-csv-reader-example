b197e579f64c258982765b877c0cc04079c167a1
Thomas <hollandpirates@gmail.com>
2019-10-06 10:42:47 +0200

    csv reader example

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-group = "deltadak"
-version = "0.0"
-
 plugins {
 
     val kotlinVersion = "1.3.11"
@@ -14,12 +11,6 @@ plugins {
 
     // Plugin which can update Gradle dependencies, use help/useLatestVersions
     id("se.patrikerdes.use-latest-versions") version "0.2.12"
-
-    // Test coverage
-    jacoco
-
-    // Upload jacoco coverage reports to coveralls
-    id("com.github.kt3k.coveralls") version "2.8.4"
 }
 
 application {
@@ -30,61 +21,12 @@ dependencies {
     compile(kotlin("stdlib"))
     // To "prevent strange errors".
     compile(kotlin("reflect"))
-    // Kotlin reflection.
-    compile(kotlin("test"))
-    compile(kotlin("test-junit"))
 
-    // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
-    testRuntime("org.junit.platform:junit-platform-console:1.5.1")
-
-    // Kotlintest
-    testCompile("io.kotlintest:kotlintest-core:3.4.0")
-    testCompile("io.kotlintest:kotlintest-assertions:3.4.0")
-    testCompile("io.kotlintest:kotlintest-runner-junit5:3.4.0")
-
-    // JavaFX tests using TestFX
-    testCompile("org.testfx:testfx-core:4.0.16-alpha")
-    testCompile("org.testfx:testfx-junit:4.0.16-alpha")
-    // Only needed for headless testing.
-//    testCompile("org.testfx:openjfx-monocle:8u76-b04") // jdk-9+181 for Java 9
-
-    // Spek
-    testCompile("org.spekframework.spek2:spek-dsl-jvm:2.0.6")
-    testRuntime("org.spekframework.spek2:spek-runner-junit5:2.0.6")
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:0.7.3")
 }
 
 repositories {
     mavenCentral()
     mavenLocal()
     jcenter()
-}
-
-// Test coverage reporting for coveralls.
-tasks {
-    // Enable xml for coveralls.
-    "jacocoTestReport"(JacocoReport::class) {
-        reports {
-            // To be read by humans
-            html.isEnabled = true
-            // To be read by Coveralls etc.
-            xml.isEnabled = true
-            xml.destination = file("$buildDir/reports/jacoco/test/jacocoTestReport.xml")
-        }
-
-    }
-
-    // Trying to run tests every time.
-    val test by tasks
-    val cleanTest by tasks
-    test.dependsOn(cleanTest)
-
-    // Use the built-in JUnit support of Gradle.
-    "test"(Test::class) {
-        useJUnitPlatform()
-    }
-
-    // Sorry, I have no idea.
-    Unit
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "kotlin-template-project"
+rootProject.name = "kotlin-csv-reader-example"
