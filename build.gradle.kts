99aeb237c7f98d9f84bce137ff2b519967eb443e
Thomas <hollandpirates@gmail.com>
2020-07-02 15:22:23 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.28.0"
 
     // Plugin which can update Gradle dependencies, use help/useLatestVersions
-    id("se.patrikerdes.use-latest-versions") version "0.2.13"
+    id("se.patrikerdes.use-latest-versions") version "0.2.14"
 }
 
 application {
