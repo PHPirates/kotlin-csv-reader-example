72fac8d8db986fefd8d72ffb381a455e275079e3
Thomas Schouten <hollandpirates@gmail.com>
2022-08-13 18:10:48 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
 plugins {
-
-    val kotlinVersion = "1.3.61"
-
     application
-    kotlin("jvm") version kotlinVersion
+    kotlin("jvm") version("1.7.0")
     java // Required by at least JUnit.
 
     // Plugin which checks for dependency updates with help/dependencyUpdates task.
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 
     // Plugin which can update Gradle dependencies, use help/useLatestVersions
-    id("se.patrikerdes.use-latest-versions") version "0.2.17"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
 }
 
 dependencies {
@@ -18,7 +15,7 @@ dependencies {
     // To "prevent strange errors".
     implementation(kotlin("reflect"))
 
-    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:0.15.2")
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.6.0")
 }
 
 repositories {
